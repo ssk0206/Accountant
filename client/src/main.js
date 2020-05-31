ddd96a387677cdaaa4cbb306910e32b7ae5e4304
SSK0206 <coccinelle.sasa@gmail.com>
2020-05-31 17:28:45 +0900

    axios 設定

--- client/src/main.js
+++ client/src/main.js
@@ -4,11 +4,24 @@ import router from './router'
 import ElementUI from 'element-ui'
 import locale from 'element-ui/lib/locale/lang/ja'
 import VueGoodTablePlugin from 'vue-good-table'
+// import axios from 'axios'
+import VueAxios from 'vue-axios'
+
 import 'vue-good-table/dist/vue-good-table.css'
 
 import './styles.scss'
 
+const axiosBase = require('axios');
+const axios = axiosBase.create({
+  baseURL: 'http://localhost:8080', // バックエンドB のURL:port を指定する
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  responseType: 'json'
+});
+
 Vue.use(ElementUI, { locale })
+Vue.use(VueAxios, axios)
 Vue.use(VueGoodTablePlugin)
 
 Vue.config.productionTip = false
