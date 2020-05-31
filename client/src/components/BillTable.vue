5cb9c989c4865c558bfae829c776c37e00271fb6
SSK0206 <coccinelle.sasa@gmail.com>
2020-05-31 18:58:15 +0900

    BillTableに変更

---
+++ client/src/components/BillTable.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+    <vue-good-table
+      :columns="columns"
+      :rows="rows"/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'my-component',
+  data(){
+    return {
+      columns: [
+        {
+          label: 'Name',
+          field: 'name',
+        },
+        {
+          label: '部屋番号',
+          field: 'roomid',
+          type: 'number',
+        },
+        {
+          label: '前のメーター値',
+          field: 'pre_meter_val',
+          type: 'number',
+        },
+        {
+          label: '今のメーター値',
+          field: 'new_meter_val',
+          type: 'number',
+        },
+        {
+          label: '電気料金',
+          field: 'bill',
+          type: 'number',
+        },
+      ],
+      rows: [],
+    };
+  },
+  created: function() {
+    this.getAllData()
+  },
+  methods: {
+    getAllData() {
+      this.axios.get("/students")
+      .then(response => {
+        if (response.status != 200) {
+          console.log("レスポンスエラー")
+        } else {
+          this.rows = response.data
+        }
+      })
+    }
+  },
+};
+</script>
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <Admin msg="Welcome to Your Vue.js App"/>
+    <bill-table msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import Admin from '@/components/Admin.vue'
+import BillTable from '@/components/BillTable.vue'
 
 export default {
   name: 'Home',
   components: {
-    Admin
+    BillTable
   }
 }
 </script>
