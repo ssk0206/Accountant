aef3f14525418f1a3f88c4a6e0a82189cb48042d
SSK0206 <coccinelle.sasa@gmail.com>
2020-05-31 13:13:02 +0900

    first commit

---
+++ client/src/main.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import ElementUI from 'element-ui'
+import locale from 'element-ui/lib/locale/lang/ja'
+import './styles.scss'
+
+Vue.use(ElementUI, { locale })
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')
