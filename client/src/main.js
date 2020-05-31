0e4e9f1dac4c82db6b157beb6a25f0d76432d9d5
SSK0206 <coccinelle.sasa@gmail.com>
2020-05-31 14:56:36 +0900

    HomeコンポーネントにTable表示

--- client/src/main.js
+++ client/src/main.js
@@ -3,9 +3,13 @@ import App from './App.vue'
 import router from './router'
 import ElementUI from 'element-ui'
 import locale from 'element-ui/lib/locale/lang/ja'
+import VueGoodTablePlugin from 'vue-good-table'
+import 'vue-good-table/dist/vue-good-table.css'
+
 import './styles.scss'
 
 Vue.use(ElementUI, { locale })
+Vue.use(VueGoodTablePlugin)
 
 Vue.config.productionTip = false
 
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Admin msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Admin from '@/components/Admin.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    Admin
   }
 }
 </script>
